f580d7e4b01c6585643d40a5c244b86e63426f7e
ibrakhimus <randizzys@gmail.com>
2024-03-23 22:10:01 -0400

    mediun block, moving cards

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -13,8 +13,17 @@ import {
 
 import Focus from "@/components/focus";
 import CarouselBlock from "@/components/carousel-content";
+import MediumBlock from "@/components/medium-block";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 
 export default function test() {
+  const mediumBlockDetails = {
+    imgUrl: "https://example.com/image.jpg",
+    title: "Example Title",
+    description:
+      "This is an example description for the MediumBlock component.",
+  };
+
   const cardsData = [
     {
       imgUrl:
@@ -46,6 +55,11 @@ export default function test() {
 
   return (
     <div className="flex flex-col items-center justify-center">
+      <InfiniteMovingCards
+        items={testimonials}
+        direction="right"
+        speed="slow"
+      />
       {/* <Carousel className="w-full max-w-sm">
         <CarouselContent className="-ml-1">
           {Array.from({ length: 5 }).map((_, index) => (
@@ -89,6 +103,39 @@ export default function test() {
       <div className="mb-20"></div>
       <Focus focusDetails={focusDetails} />
       <div className="mb-20"></div>
+      <MediumBlock mediumBlockDetails={mediumBlockDetails} />
     </div>
   );
 }
+
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
